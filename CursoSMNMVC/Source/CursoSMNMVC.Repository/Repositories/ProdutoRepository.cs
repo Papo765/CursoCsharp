1a93b5bdfadcf50b87c84bc7707a3fabc5e65e80
João Paulo <joaopauloamesco@gmail.com>
2018-01-29 20:50:57 -0200

    finalizando a camada de repositorio

---
+++ CursoSMNMVC/Source/CursoSMNMVC.Repository/Repositories/ProdutoRepository.cs
@@ -0,0 +1,33 @@
+﻿using CursoSMNMVC.Domain.Entidades;
+using CursoSMNMVC.Repository.DataBase;
+using ProjetoCursoFeriasSMN.Repository.DataBase;
+using System.Collections.Generic;
+
+
+namespace CursoSMNMVC.Repository.Repositories
+{
+    public class ProdutoRepository : Execucao
+    {
+        private static readonly Conexao conexao = new Conexao();
+
+        public ProdutoRepository(Conexao conexao) : base(conexao)
+        {
+        }
+
+        public IEnumerable<Produto> GetProdutos()
+        {
+            ExecuteProcedure("[dbo].SelProdutos]");
+
+            var produtos = new List<Produto>();
+            using ( var reader = ExecuteReader())
+            {
+                while (reader.Read())
+                    produtos.Add(new Produto
+                    {
+                        Nome = reader.ReadAsString("Nome")
+                    });
+            }
+            return produtos;
+        }
+    }
+}
